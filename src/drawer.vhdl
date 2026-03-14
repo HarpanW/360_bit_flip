4fa37ab312dbc5c1fc059a31b01f624ec1b1f203
aromo613 <aromo613@student.liu.se>
2026-03-14 12:32:47 +0100

    Add initialisation of the LED grid display

--- src/drawer.vhdl
+++ src/drawer.vhdl
@@ -13,30 +13,13 @@ entity drawer is
 end drawer;
 
 architecture Behavioral of drawer is
-    signal row : std_logic_vector (1 downto 0);
-    signal cnt : std_logic_vector (3 downto 0);
+    signal row : unsigned (1 downto 0);
+    signal cnt : unsigned (3 downto 0);
     signal curr_word : std_logic_vector (15 downto 0);
-    signal get_word : std_logic;
-    signal init     : std_logic_vector (1 downto 0) := "11";
+    signal get_word : std_logic := '1';
+    signal init     : unsigned (2 downto 0) := "100";
 
 
-    procedure shift_data(
-        signal   counter   : inout integer;
-        signal   word      : inout std_logic_vector(15 downto 0);
-        signal   data_bit  : out   std_logic;
-        signal   get_next  : out   std_logic
-    ) is
-    begin
-        if counter = 15 then
-            get_next <= '1';
-        end if;
-        counter <= counter + 1; -- Overflows
-        cs <= '1';
-
-        -- Skifta ut MSB
-        data_bit <= word(15);
-        word <= word(14 downto 0) & '0';
-    end procedure;
 
 begin
     -- Game loop
@@ -47,30 +30,54 @@ begin
 
         
 
-    process(clk)
+    process(clk, rst_n)
     begin
     if rst_n = '1' then
         curr_word <= (others => '0');
         cnt <= (others => '0');
         get_word <= '1';
-        cs <= '1';
-        init <= "11";
+        cs <= '1'; 
+        row <= "00";
+        init <= "100";
+        data_out <= '0';
+
     end if;
     if rising_edge(clk) then
-        if init = 3 then -- Subrutin för att slå på kretsen
-            if cnt = 0 then
-                curr_word <= x"0C01"; -- Slå på kretsen
-            end if;
-            shift_data(cnt, curr_word, data_out, init);
-
         
-        elsif get_word = '1' then
-            curr_word <= "0000" & std_logic_vector(unsigned(row) + 2)(3 downto 0) & "00" & data_in(unsigned(row)*4 + 3 downto unsigned(row)*4) & "00"; -- pro
-            row <= row + 1; -- Overflows
+        if get_word = '1' then
             cs <= '0';
             get_word <= '0';
+            cnt <= (others => '0');
+            -- ONLY RUNS WHEN INITIALISING THE SCREEN
+            if init = 4 then 
+                curr_word <= x"0C01"; -- Slå på kretsen
+                init <= init - 1;
+            elsif init = 3 then 
+                curr_word <= x"0B07"; -- Ange att 8 siffror används
+                init <= init - 1;
+            elsif init = 2 then
+                curr_word <= x"0900"; -- Vi anger varje pixel manuellt (Oklart)
+                init <= init - 1;
+            elsif init = 1 then 
+                curr_word <= x"0A0F"; -- Maximal ljusstyrka
+                init <= init - 1;
+            
+            -- Normal game loop, above is init
+            else  
+                curr_word <= "0000" & std_logic_vector(row + 2)(3 downto 0) & "00" & data_in(row*4 + 3 downto row*4) & "00"; -- pro
+                row <= row + 1; -- Overflows
+            end if;
         else
-            shift_data(cnt, curr_word, data_out, get_word);
+            -- Skiftregister
+            if cnt = 15 then
+                get_word <= '1';
+            end if;
+            cnt <= cnt + 1; -- Overflows
+            cs <= '1';
+
+            -- Skifta ut MSB
+            data_out <= curr_word(15);
+            curr_word <= curr_word(14 downto 0) & '0';
         end if;
     end if;
     end process;
